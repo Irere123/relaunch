70af5044833fdf0e643a6d0f79c6746aa84b67e0
Irere123 <irere2020@outlook.com>
2024-10-19 10:46:08 +0200

    feat: redesign login page

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -2,7 +2,7 @@ import SignIn from "@/components/sign-in";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center w-full min-h-screen sm:px-0 px-3">
       <SignIn />
     </div>
   );
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import { signIn } from "@/modules/auth";
+import Link from "next/link";
 
 export default function SignIn() {
   return (
@@ -7,8 +10,27 @@ export default function SignIn() {
         "use server";
         await signIn("google", { redirectTo: "/dashboard" });
       }}
+      className="max-w-md border border-gray-200 rounded-md shadow-lg"
     >
-      <button type="submit">Signin with Google</button>
+      <div className="flex flex-col items-center justify-center space-y-3 border-b px-4 py-6 pt-8 text-center md:px-16">
+        <Link href="/">
+          <Image
+            src="/relaunch.svg"
+            alt="Logo"
+            className="h-10 w-10 rounded-full"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <h3 className="font-display text-2xl font-bold">Get started!</h3>
+        <p className="text-sm text-gray-500">
+          To submit your project to the Relaunch, create a free account by
+          signing in with Google.
+        </p>
+      </div>
+      <div className="flex flex-col space-y-4 bg-gray-50 px-4 rounde-b-md py-8 md:px-16">
+        <button type="submit">Sign in with Google</button>
+      </div>
     </form>
   );
 }
