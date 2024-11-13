d5ba3fc687e346109c45269a5b82b618fd9fce59
Irere123 <irere2020@outlook.com>
2024-11-13 15:03:32 +0200

    refactor: go back button

--- src/app/(docs)/why-build-this/page.tsx
+++ src/app/(docs)/why-build-this/page.tsx
@@ -1,13 +1,18 @@
+import { ChevronLeft } from "lucide-react";
 import { WhyBuildThisPageClient } from "./page.client";
+import Link from "next/link";
 
 export const metadata = {
-  title: "Why Build This?", 
+  title: "Why Build This?",
 };
-
 
 export default function WhyBuildThisPage() {
   return (
     <>
+      <Link href="/" className="flex items-center p-4">
+        <ChevronLeft />
+        <span className="ml-2">Go home</span>
+      </Link>
       <WhyBuildThisPageClient />
     </>
   );
